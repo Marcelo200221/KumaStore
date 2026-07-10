0e10e6c6bda3d51ab3fad6b8b91f84adbb0bde07
Marcelo <ma.darras@duocuc.cl>
2026-07-10 16:39:19 -0400

    Se corrige error en configuracion de database

--- kumaStore/settings.py
+++ kumaStore/settings.py
@@ -97,8 +97,8 @@ WSGI_APPLICATION = "kumaStore.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url(
-        default = 'sqlite://db.sqlite3',
+    "default": dj_database_url.config(
+        default = os.environ.get('DATABASE_URL'),
         conn_max_age = 600
     )
 }
