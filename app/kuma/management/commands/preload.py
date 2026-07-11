adab1157ed459d7be078fba0d5c99d0c25827f03
Marcelo <ma.darras@duocuc.cl>
2026-07-11 14:21:49 -0400

    Se corrige URL de imagenes para los productos, y se modifican llamadas de imagenes y funciones en registro

--- app/kuma/management/commands/preload.py
+++ app/kuma/management/commands/preload.py
@@ -24,20 +24,20 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS("Categorias procesadas..."))
 
         productos = [
-            {"sku": 1, "nombre": "Acana", "descripcion": "Comida para perros de la marca Acana", "stock": 24, "precio": 5000, "fecha_vencimiento": "13/03/2028", "categoria_id": 8, "imagen_url": "media/imagenesProductos/Acana.webp"},
-            {"sku": 2, "nombre": "Diamond natural", "descripcion": "Comida para perros de la marca Diamond natural", "stock": 50, "precio": 10000, "fecha_vencimiento": "13/04/2028", "categoria_id": 8, "imagen_url": "media/imagenesProductos/diamond-natural-adult-small-breed.webp"},
-            {"sku": 3, "nombre": "Prestige", "descripcion": "Comida para perros marca Prestige", "stock": 45, "precio": 20000, "fecha_vencimiento": "25/11/2029", "categoria_id": 8, "imagen_url": "media/imagenesProductos/Prestige.webp"},
-            {"sku": 4, "nombre": "Bandana Escocesa", "descripcion": "Bandana para mascotas con patron Escoces", "stock": 24, "precio": 5000, "categoria_id": 4, "imagen_url": "media/imagenesProductos/bandanaescocesa_300x.webp"},
-            {"sku": 5, "nombre": "Bandana Playera", "descripcion": "Bandana con diseño playero", "stock": 15, "precio": 7000, "categoria_id": 4, "imagen_url": "media/imagenesProductos/bandanaplayera.avif"},
-            {"sku": 6, "nombre": "Bandana Popcorn", "descripcion": "Bandana con divertido diseño de PopCorn", "stock": 35, "precio": 2500, "categoria_id": 4, "imagen_url": "media/imagenesProductos/popcorn_bandana.avif"},
-            {"sku": 7, "nombre": "Peluche", "descripcion": "Peluche felpudo para jugar", "stock": 30, "precio": 7500, "categoria_id": 7, "imagen_url": "media/imagenesProductos/Peluche_de_animal.jpg"},
-            {"sku": 8, "nombre": "Pantufla", "descripcion": "Peluche con forma de pantufla para aquellas mascotas que adoran destruirlas", "stock": 24, "precio": 5000, "categoria_id": 7, "imagen_url": "media/imagenesProductos/Peluche_pantufla.jpg"},
-            {"sku": 9, "nombre": "Juguete chillon", "descripcion": "Tipico juguete chillon para que tu mascota se entretenga", "stock":15, "precio": 6500, "categoria_id": 7, "imagen_url": "media/imagenesProductos/Juguete_Chillon.jpg"},
-            {"sku": 10, "nombre": "Collar Stranger Things", "descripcion": "Collar con diseño de la famosa serie Stranger Things", "stock": 50, "precio": 15000, "categoria_id": 5, "imagen_url": "media/imagenesProductos/Collar_stranger_things.jpg"},
-            {"sku": 11, "nombre": "Arnes de torso", "descripcion": "Arnes que cubre el torso de tu mascota y asegura un paseo sin ahorcamientos", "stock": 15, "precio": 10000, "categoria_id": 5, "imagen_url": "media/imagenesProductos/mascan-arnés-cruzado-acolchado.jpg"},
-            {"sku": 12, "nombre": "Arnes acolchado", "descripcion": "Arnes con dos puntos de contacto acolchado", "stock": 30, "precio": 5000, "categoria_id": 5, "imagen_url": "media/imagenesProductos/Arnes_acolchado.jpg"},
-            {"sku": 13, "nombre": "Colgante Safety Light", "descripcion": "Colgante con luz y sonido para encontrar a tu mascota en caso de emergencia", "stock": 20, "precio": 20000, "categoria_id": 6, "imagen_url": "media/imagenesProductos/Colgante_safety_light.jpg"},
-            {"sku": 14, "nombre": "Colgante Antipulgas", "descripcion": "Colgante que ayuda a prevenir las pulgas", "stock": 24, "precio": 15000, "categoria_id": 6, "imagen_url": "media/imagenesProductos/Colgante_Antipulgas_y_garrapatas.png"},
+            {"sku": 1, "nombre": "Acana", "descripcion": "Comida para perros de la marca Acana", "stock": 24, "precio": 5000, "fecha_vencimiento": "13/03/2028", "categoria_id": 8, "imagen_url": "imagenesProductos/Acana.webp"},
+            {"sku": 2, "nombre": "Diamond natural", "descripcion": "Comida para perros de la marca Diamond natural", "stock": 50, "precio": 10000, "fecha_vencimiento": "13/04/2028", "categoria_id": 8, "imagen_url": "imagenesProductos/diamond-natural-adult-small-breed.webp"},
+            {"sku": 3, "nombre": "Prestige", "descripcion": "Comida para perros marca Prestige", "stock": 45, "precio": 20000, "fecha_vencimiento": "25/11/2029", "categoria_id": 8, "imagen_url": "imagenesProductos/Prestige.webp"},
+            {"sku": 4, "nombre": "Bandana Escocesa", "descripcion": "Bandana para mascotas con patron Escoces", "stock": 24, "precio": 5000, "categoria_id": 4, "imagen_url": "imagenesProductos/bandanaescocesa_300x.webp"},
+            {"sku": 5, "nombre": "Bandana Playera", "descripcion": "Bandana con diseño playero", "stock": 15, "precio": 7000, "categoria_id": 4, "imagen_url": "imagenesProductos/bandanaplayera.avif"},
+            {"sku": 6, "nombre": "Bandana Popcorn", "descripcion": "Bandana con divertido diseño de PopCorn", "stock": 35, "precio": 2500, "categoria_id": 4, "imagen_url": "imagenesProductos/popcorn_bandana.avif"},
+            {"sku": 7, "nombre": "Peluche", "descripcion": "Peluche felpudo para jugar", "stock": 30, "precio": 7500, "categoria_id": 7, "imagen_url": "imagenesProductos/Peluche_de_animal.jpg"},
+            {"sku": 8, "nombre": "Pantufla", "descripcion": "Peluche con forma de pantufla para aquellas mascotas que adoran destruirlas", "stock": 24, "precio": 5000, "categoria_id": 7, "imagen_url": "imagenesProductos/Peluche_pantufla.jpg"},
+            {"sku": 9, "nombre": "Juguete chillon", "descripcion": "Tipico juguete chillon para que tu mascota se entretenga", "stock":15, "precio": 6500, "categoria_id": 7, "imagen_url": "imagenesProductos/Juguete_Chillon.jpg"},
+            {"sku": 10, "nombre": "Collar Stranger Things", "descripcion": "Collar con diseño de la famosa serie Stranger Things", "stock": 50, "precio": 15000, "categoria_id": 5, "imagen_url": "imagenesProductos/Collar_stranger_things.jpg"},
+            {"sku": 11, "nombre": "Arnes de torso", "descripcion": "Arnes que cubre el torso de tu mascota y asegura un paseo sin ahorcamientos", "stock": 15, "precio": 10000, "categoria_id": 5, "imagen_url": "imagenesProductos/mascan-arnés-cruzado-acolchado.jpg"},
+            {"sku": 12, "nombre": "Arnes acolchado", "descripcion": "Arnes con dos puntos de contacto acolchado", "stock": 30, "precio": 5000, "categoria_id": 5, "imagen_url": "imagenesProductos/Arnes_acolchado.jpg"},
+            {"sku": 13, "nombre": "Colgante Safety Light", "descripcion": "Colgante con luz y sonido para encontrar a tu mascota en caso de emergencia", "stock": 20, "precio": 20000, "categoria_id": 6, "imagen_url": "imagenesProductos/Colgante_safety_light.jpg"},
+            {"sku": 14, "nombre": "Colgante Antipulgas", "descripcion": "Colgante que ayuda a prevenir las pulgas", "stock": 24, "precio": 15000, "categoria_id": 6, "imagen_url": "imagenesProductos/Colgante_Antipulgas_y_garrapatas.png"},
         ]
 
         
