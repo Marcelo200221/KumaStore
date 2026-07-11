183630e6977c32a168ce07354ed87b6ccc2faceb
Marcelo <ma.darras@duocuc.cl>
2026-07-11 14:23:55 -0400

    Se corrigen {} por [] en STATICFILES_DIRS en setting.py

--- kumaStore/settings.py
+++ kumaStore/settings.py
@@ -137,10 +137,10 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),
-}
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
