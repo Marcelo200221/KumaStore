73d9e3a4445cfd8b69757971d1c146c882ab5c90
Marcelo <ma.darras@duocuc.cl>
2026-07-11 13:51:53 -0400

    Se agregan mensajes de error para comandos de migracion y poblacion de datos a la fuerza

--- build.sh
+++ build.sh
@@ -6,13 +6,33 @@ set -o errexit
 pip install -r requirements.txt
 
 # 2. Recopilar archivos estáticos
-python manage.py collectstatic --no-input
+echo "=== RECOPILANDO ARCHIVOS ESTATICOS ==="
+if !python manage.py collectstatic --no-input; then
+    echo "ERROR CRITICO: El comando 'collectstatic' fallo. Rstreo del error:"
+    python manage.py collectstatic --no-input 2>&1
+    exit 1
+fi
 
 # 3. Crear los archivos de migración en el servidor (por si acaso)
-python manage.py makemigrations --no-input
+echo "=== INICIANDO MIGRACIONES EN PRODUCCIÓN ==="
+if ! python manage.py makemigrations --no-input; then
+    echo "❌ ERROR CRÍTICO: El comando 'makemigrations' falló. Rastreo del error:"
+    python manage.py makemigrations --no-input 2>&1
+    exit 1
+fi
 
 # 4. Aplicar las migraciones a Supabase (creará las tablas que faltan)
-python manage.py migrate --no-input
-
+echo "=== INICIANDO MIGRACIONES EN PRODUCCIÓN ==="
+if ! python manage.py migrate --no-input; then
+    echo "❌ ERROR CRÍTICO: El comando 'migrate' falló. Rastreo del error:"
+    python manage.py migrate --no-input 2>&1
+    exit 1
+fi
 # 5. Cargar los productos y categorías
-python manage.py preload
+echo "=== INICIANDO SCRIPT PRELOAD ==="
+if ! python manage.py preload; then
+    echo "❌ ERROR CRÍTICO: El comando 'preload' falló. Rastreo del error:"
+    python manage.py preload 2>&1
+    exit 1
+fi
+echo "=== PROCESO DE CONSTRUCCIÓN FINALIZADO CON ÉXITO ==="
